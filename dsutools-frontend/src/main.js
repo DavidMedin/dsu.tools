9c06fa32ce7865e2af47c09285b7228cecf5255e
David Medin <david@davidmedin.com>
2024-09-14 13:00:23 -0500

    Initial vue project setup.
    Doesn't contains any dsu.tools specific content, but is just a vue project template.

---
+++ dsutools-frontend/src/main.js
@@ -0,0 +1,10 @@
+// This file will be imported by index.html.
+
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// '#app' is a element selector that references the <div> in index.html.
+// Our Vue code will be inserted into that div.
+createApp(App).mount('#app')
