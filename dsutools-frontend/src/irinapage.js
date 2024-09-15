4ee54dc1a96af92233d43645bdc5544c579a3b56
irinapech <120232888+irinapech@users.noreply.github.com>
2024-09-15 15:58:14 -0500

    added irina page

---
+++ dsutools-frontend/src/irinapage.js
@@ -0,0 +1,8 @@
+// This file will be imported by irinapage.html
+
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import IrinaPage from './IrinaPage.vue'
+
+createApp(IrinaPage).mount('#irinapage')
