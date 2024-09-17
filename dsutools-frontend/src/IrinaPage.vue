70ebaf994415e8a19a83cb1c3c28081ad4cc5c2d
irinapech <120232888+irinapech@users.noreply.github.com>
2024-09-17 17:44:57 -0500

    arranged the main blocks on the page

--- dsutools-frontend/src/IrinaPage.vue
+++ dsutools-frontend/src/IrinaPage.vue
@@ -3,11 +3,18 @@
 
 <template>
     <main>
-        <div>Tool1</div>
-        <div>Tool2</div>
-        <div>Tool3</div>
-        <div>Tool4</div>
-        <div>Tool5</div>
+        <div id="dsu-tools"           class="box">DSU TOOLS</div>
+        <div id="calculator"          class="box">CALCULATOR
+            <!-- <i class="arrow right"></i> -->
+        </div>
+        <div id="events"              class="box">EVENTS</div>
+        <div id="puzzle"              class="box">PUZZLE</div>
+        <div class="scientific-calculators">
+            <div id="vitamins-calculator" class="box">VITAMINS CALCULATOR</div>
+            <div id="molecules-calculator">MOLECULES CALCULATOR</div>
+        </div>
+        <div id="todays-menu"         class="box">TODAYS MENU</div>
+        <div id="flashcards"          class="box">FLASHCARDS</div>
     </main>
 </template>
 
@@ -15,21 +22,91 @@
     * {
         font-family: 'Courier New', Courier, monospace;
         color: black;
+        background-color: var(--dark-blue);
     }
 
     main {
         display: flex;
-        margin: 3rem;
-        gap: 2rem;
         align-items: center;
         justify-content: center;
         flex-wrap: wrap;
+        position: absolute;
     }
+
     div {
-        background-color: burlywood;
-        width: 20rem;
+        display: flex;
+        border-radius: 5rem;
+        justify-content: center;
+        align-items: center;
+        margin: 0.5rem 1rem 0.5rem 1rem;
+        font-size: 2rem;
+        color: var(--white);
+    }
+
+    .scientific-calculators {
+        display: flex;
+        flex-direction: column;
+    }
+
+    #dsu-tools,
+    #calculator,
+    #events,
+    #vitamins-calculator,
+    #molecules-calculator 
+    {
         height: 20rem;
-        border-radius: 15%;
-        text-align: center;
+    } 
+
+    #dsu-tools, 
+    #puzzle
+    {
+        width: 20rem;
+    }
+
+    #calculator,
+    #vitamins-calculator,
+    #molecules-calculator
+    {
+        width: 30rem;
+    }
+
+    #events {
+        width: 40rem;
+    }
+
+    #puzzle,
+    #todays-menu,
+    #flashcards,
+    .scientific-calculators {
+        height: 40rem;
+    }
+
+    #dsu-tools {
+        background-color: var(--white);
+        color: var(--pink-text);
+    }
+
+    #calculator,
+    #events,
+    #molecules-calculator {
+        background-color: var(--pink-background);
+    }
+
+    #puzzle, 
+    #vitamins-calculator    {
+        background-color: var(--blue);
+    }
+
+    #flashcards {
+        background-color: var(--light-pink-background);
+        width: 19.5rem;
+    }
+
+    #todays-menu{
+        background-color: var(--blue);
+        width: 19.5rem;
+    }
+    .scientific-calculators {
+        margin: 0;
     }
 </style>
