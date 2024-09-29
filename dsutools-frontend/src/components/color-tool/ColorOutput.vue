c6d013cc49ebc94f0f892e7517282fbc5be6cd38
David Medin <david@davidmedin.com>
2024-09-29 14:04:05 -0500

    commit to move changes to laptop

--- dsutools-frontend/src/components/color-tool/ColorOutput.vue
+++ dsutools-frontend/src/components/color-tool/ColorOutput.vue
@@ -1,4 +1,5 @@
 <script setup>
+import CopyableText from "../CopyableText.vue"
 </script>
 
 <template>
@@ -6,22 +7,22 @@
     <thead>
         <tr>
             <th scope="col">Color Space</th>
-            <th scope="col">Components</th>
+            <th scope="col" colspan=3>Components</th>
         </tr>
     </thead>
     <tbody>
         <tr>
-            <td>RGB</td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
+            <td><CopyableText text="RGB"/></td>
+            <td><CopyableText text="Red: 1"/></td>
+            <td><CopyableText text="Green: 2"/></td>
+            <td><CopyableText text="Blue: 3"/></td>
         </tr>
 
         <tr>
-            <td>HCT</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
+            <td><CopyableText text="HCT"/></td>
+            <td><CopyableText text="Hue: 2"/></td>
+            <td><CopyableText text="Chroma: 3"/></td>
+            <td><CopyableText text="Tone: 4"/></td>
         </tr>
     </tbody>
 </table>
