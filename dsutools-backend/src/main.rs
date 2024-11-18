cc31db67a6656e66ad50dd1730d79ab9ea2eef85
David Medin <david@davidmedin.com>
2024-11-18 13:30:13 -0600

    Cleaned some Rust code.

--- dsutools-backend/src/main.rs
+++ dsutools-backend/src/main.rs
@@ -1,16 +1,19 @@
 #[macro_use]
 extern crate rocket;
 
+// Importing libraries ====================
 use rocket::fs::FileServer;
 use rocket::http::Status;
 use rocket::serde::{json::Json, Deserialize, Serialize};
 
-use rocket_db_pools::sqlx::{self, Row};
+use rocket_db_pools::sqlx;
 use rocket_db_pools::{Connection, Database};
 
-#[derive(Database)]
-#[database("dsutools_db")]
-struct DsuToolsDB(sqlx::SqlitePool);
+// =========================================
+
+#[derive(Database)] // A Rust macro that operates on the DsuToolsDB struct.
+#[database("dsutools_db")] // Links the DsuToolsDB struct to the "dsutools_db" database mentioned in the Rocket.toml file.
+struct DsuToolsDB(sqlx::SqlitePool); // A struct with one field, a SqlitePool.
 
 #[derive(Deserialize, Debug)]
 struct User {
@@ -30,12 +33,23 @@ async fn login(
     user: Json<User>,
     mut db: Connection<DsuToolsDB>,
 ) -> Result<Json<LoginResponse>, Status> {
-    _ = sqlx::query("SELECT * FROM users WHERE username = ? AND password_hash = ?")
+    // This function returns a Json<LoginResponse> on success or a Status code on failure.
+
+    // Generate a SQL query
+    let query = sqlx::query("SELECT * FROM users WHERE username = ? AND password_hash = ?")
         .bind(&user.username)
-        .bind(&user.password)
-        .fetch_one(&mut **db)
-        .await
-        .map_err(|_| Status::Unauthorized)?;
+        .bind(&user.password);
+
+    // Perform the query to the database.
+    let query_result = query.fetch_one(&mut **db).await; // Asyncronous Rust keyword. This function will wait for the database to respond.
+
+    // query_result is a Result type, which can be Ok(<some value>) or Err(<some value>).
+    // If it is an error, ...
+    if let Err(_) = query_result {
+        // If the query fails, return a HTTP 401 Unauthorized status code.
+        return Err(Status::Unauthorized);
+    }
+
     let response = LoginResponse {
         token: "A big long token".to_string(),
     };
@@ -49,7 +63,7 @@ fn failed_login() -> Status {
     return Status::Unauthorized;
 }
 
-// 'rocket:main' is another macro that tells Rocket that this is our main function.
+// 'rocket::main' is another macro that tells Rocket that this is our main function.
 // While compiling, Rocket will modify this function using witchcraft.
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
@@ -57,7 +71,7 @@ async fn main() -> Result<(), rocket::Error> {
     // we've defined above, and where to mount it.
     // Finally, it runs the server until the server is stopped.
     let _rocket = rocket::build()
-        .attach(DsuToolsDB::init())
+        .attach(DsuToolsDB::init()) // Use this database.
         .mount("/", routes![login, failed_login])
         .mount("/", FileServer::from("../dsutools-frontend/dist/"))
         .launch()
