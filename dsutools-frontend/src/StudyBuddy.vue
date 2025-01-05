e24534571ac4de31e01be35c12789b5d321372cd
irinapech <120232888+irinapech@users.noreply.github.com>
2025-01-05 15:13:43 -0600

    more code optimization

--- dsutools-frontend/src/StudyBuddy.vue
+++ dsutools-frontend/src/StudyBuddy.vue
@@ -7,11 +7,12 @@ import FlashcardSetOptions from "./components/FlashcardSetOptions.vue";
 const flashcards = ref([]);
 const savedSets = ref([]);
 const selectedSet = ref(null);
-const isFormVisible = ref(false);
+const isFlashcardSetFormVisible = ref(false);
+const isFlashcardFormVisible = ref(false);
 const errorMessage = ref("");
 
 function toggleVisibility() {
-    isFormVisible.value = !isFormVisible.value;
+    isFlashcardSetFormVisible.value = !isFlashcardSetFormVisible.value;
 }
 
 function selectSet(set) {
@@ -43,7 +44,7 @@ function fetchDecksFlashcards(deckName) {
 }
 
 // fetch the saved flashcard decks
-function displaySavedFlashcardSets() {
+function fetchSavedFlashcardSets() {
     if (localStorage.getItem("username") == null) {
         alert("Please log in to view your flashcard sets!");
         return;
@@ -62,11 +63,11 @@ function displaySavedFlashcardSets() {
             console.error("Error: ", error);
         });
 }
-
 onMounted(() => {
-    displaySavedFlashcardSets();
+    fetchSavedFlashcardSets();
 })
 
+// create new flashcard set
 onMounted(() => {
     let newFlashcardSetForm = document.getElementById("newFlashcardSetForm");
     
@@ -132,10 +133,105 @@ onMounted(() => {
     }  
     });  
 });
+
+// create new flashcard
+onMounted(() => 
+{
+    document.getElementById("newFlashcardForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let front = document.getElementsByName("front")[0].value;
+    let back = document.getElementsByName("back")[0].value;
+    // let deckName = localStorage.getItem("currentDeck");
+
+    let username = localStorage.getItem("username");
+    if (username == null) {
+        alert("Please log in to create a flashcard!");
+        return;
+    }
+
+    if (front == "" || back == "") {
+        alert("Front and Back cannot be empty!");
+    } else {
+        flashcards.value.push({ flashcard_front: front, flashcard_back: back });            
+        document.getElementById("newFlashcardForm").reset();
+    }
+    });
+})
+
+// open new flashcard form
+function openNewFlashcardForm() {
+    isFlashcardFormVisible.value = true;
+}
+
+// close new flashcard form and save flashcards to the database
+function closeFormAndSaveFlashcards() {
+    isFlashcardFormVisible.value = false;
+
+    if (localStorage.getItem("username") == null) {
+        alert("Please log in to create a flashcard!");
+        return;
+    }
+
+    let createFlashcardsData = {
+        username: localStorage.getItem("username"),
+        flashcard_deck_name: localStorage.getItem("currentDeck"),
+        flashcards: flashcards.value,
+    }
+
+    fetch(`/create-flashcards`, {
+        method: "POST",
+        body: JSON.stringify(createFlashcardsData),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            console.log("Flashcards saved successfully!");
+        })
+        .catch(function(error) {
+            console.error("Error: ", error);
+        });
+}
 </script>
 
 <template>
-    <form class="form-popup" id="newFlashcardSetForm" :class="{ 'show': isFormVisible }" v-show="isFormVisible">
+    <Page direction="row" justifyContent="space-between" alignTimes="auto">
+        <div class="sidebar">
+            <button class="sticky-button" :set="isFlashcardSetFormVisible" @click="toggleVisibility">Create new flashcard set</button>
+            <h3 id="saved-sets-header" style="color: var(--color-primary)">SAVED SETS</h3>
+            <div id="saved-sets">
+                <div v-for="set in savedSets" 
+                    :key="set.id" 
+                    class="flashcard-set"
+                    style = "cursor: pointer;"
+                    @click="selectSet(set)"
+                >
+                    {{ set.name }}
+                </div>
+                <FlashcardSetOptions 
+                    v-if="selectedSet" 
+                    :set="selectedSet" 
+                    :deckName=selectedSet.name 
+                    :isSetSelected="true" 
+                    :functionOnClick="openNewFlashcardForm"/>
+            </div>
+        </div>
+        <div class="flashcards-main">
+            <template v-for="flashcard in flashcards" v-if="flashcards">
+                <Flashcard 
+                    :front=flashcard.flashcard_front 
+                    :back=flashcard.flashcard_back>
+                </Flashcard>
+            </template>
+        </div>
+    </Page>
+
+    <form class="form-popup" id="newFlashcardSetForm" :class="{ 'show': isFlashcardSetFormVisible }" v-show="isFlashcardSetFormVisible">
         <h1>New Flashcard Set</h1>
 
         <p v-if="errorMessage" style="color: red;">{{ errorMessage }}</p>
@@ -148,28 +244,19 @@ onMounted(() => {
         <button type="submit" class="btn">Create</button>
         <button type="button" class="btn cancel" @click="toggleVisibility">Close</button>
     </form>
-    <Page direction="row" justifyContent="space-between" alignTimes="auto">
-        <div class="sidebar">
-            <button class="sticky-button" :set="isFormVisible" @click="toggleVisibility">Create new flashcard set</button>
-            <h3 id="saved-sets-header" style="color: var(--color-primary)">SAVED SETS</h3>
-            <div id="saved-sets">
-                <div v-for="set in savedSets" 
-                :key="set.id" 
-                class="flashcard-set"
-                style.cursor="pointer"
-                @click="selectSet(set)"
-                >
-                    {{ set.name }}
-                </div>
-                <FlashcardSetOptions v-if="selectedSet" :set="selectedSet" />
-            </div>
-        </div>
-        <div class="flashcards-main">
-            <template v-for="flashcard in flashcards" v-if="flashcards">
-                <Flashcard :front=flashcard.flashcard_front :back=flashcard.flashcard_back></Flashcard>
-            </template>
-        </div>
-    </Page>
+
+    <form class="form-popup" id="newFlashcardForm" :class="{ 'show': isFlashcardFormVisible }" v-show="isFlashcardFormVisible">
+        <h1>New Flashcard</h1>
+
+        <label for="front"><b>Front</b></label>
+        <input type="text" name="front" required>
+
+        <label for="back"><b>Back</b></label>
+        <input type="text" name="back">
+
+        <button type="submit" class="btn">Create</button>
+        <button type="button" class="btn cancel" @click="closeFormAndSaveFlashcards">Close</button>
+    </form>
 </template>
 
 <style scoped>
@@ -180,6 +267,14 @@ onMounted(() => {
     align-items: center;
     width: 20%;
     gap: 1rem;
+}
+
+#saved-sets {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 0.5rem;
 }
 
 #saved-sets-header {
--- dsutools-frontend/src/components/FlashcardSetOptions.vue
+++ dsutools-frontend/src/components/FlashcardSetOptions.vue
@@ -1,93 +1,25 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
 
-const isFlashcardFormVisible = ref(false);
-
-function openNewFlashcardForm() {
-    isFlashcardFormVisible.value = true;
-}
-
-// create new flashcard
-onMounted(() => 
-{document.getElementById("newFlashcardForm").addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    let front = document.getElementsByName("front")[0].value;
-    let back = document.getElementsByName("back")[0].value;
-    let deckName = localStorage.getItem("currentDeck");
-
-    let username = localStorage.getItem("username");
-    if (username == null) {
-        alert("Please log in to create a flashcard!");
-        return;
-    }
-
-    if (front == "" || back == "") {
-        alert("Front and Back cannot be empty!");
-    } else {
-        flashcards.value.push({ flashcard_front: front, flashcard_back: back });            
-        document.getElementById("newFlashcardForm").reset();
-    }
-});})
-
-// close new flashcard form and save flashcards to the database
-function closeNewFlashcardForm() {
-    isFlashcardFormVisible.value = false;
-
-    if (localStorage.getItem("username") == null) {
-        alert("Please log in to create a flashcard!");
-        return;
-    }
-
-    let createFlashcardsData = {
-        username: localStorage.getItem("username"),
-        flashcard_deck_name: localStorage.getItem("currentDeck"),
-        flashcards: flashcards.value,
-    }
-
-    fetch(`/create-flashcards`, {
-        method: "POST",
-        body: JSON.stringify(createFlashcardsData),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-            console.log("Flashcards saved successfully!");
-        })
-        .catch(function(error) {
-            console.error("Error: ", error);
-        });
-}
+const props = defineProps({
+    isSetSelected: {
+        type: Boolean
+    },
+    deckName: {
+        type: String,
+        required: true,
+    },
+    functionOnClick: {
+        type: Function,
+        required: true,
+    },
+});
 </script>
 
 <template>
-    <button class="sticky-button" v-show="isFlashcardSetSelected" @click="openNewFlashcardForm">Create new flashcard</button>
-    
-    <form class="form-popup" id="newFlashcardForm" :class="{ 'show': isFlashcardFormVisible }">
-        <h1>New Flashcard</h1>
-
-        <label for="front"><b>Front</b></label>
-        <input type="text" name="front" required>
-
-        <label for="back"><b>Back</b></label>
-        <input type="text" name="back">
-
-        <button type="submit" class="btn">Create</button>
-        <button type="button" class="btn cancel" @click="closeNewFlashcardForm">Close</button>
-    </form>
+    <button class="sticky-button" v-show="isSetSelected" @click="props.functionOnClick">Create new flashcard</button>
+    <div>Deckname is: {{ deckName }}</div>
 </template>
 
 <style scoped>
-.form-popup {
-  display: none;
-}
-
-.form-popup.show {
-    display: flex;
-}
 </style>
